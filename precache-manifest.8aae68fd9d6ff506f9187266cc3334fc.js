228a53266d8b9348e4cbe52c10a08014bba13865
Aleksandra Półtorak <xmacabrax@gmail.com>
2018-11-03 02:23:30 +0100

    Updates

---
+++ precache-manifest.8aae68fd9d6ff506f9187266cc3334fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52435d08ff1beb8e480e",
+    "url": "/react-to-do-list/static/js/runtime~main.52435d08.js"
+  },
+  {
+    "revision": "5809be309cb1441c214a",
+    "url": "/react-to-do-list/static/js/main.5809be30.chunk.js"
+  },
+  {
+    "revision": "3a58f11f13f5574bd1fa",
+    "url": "/react-to-do-list/static/js/1.3a58f11f.chunk.js"
+  },
+  {
+    "revision": "5809be309cb1441c214a",
+    "url": "/react-to-do-list/static/css/main.632f20e0.chunk.css"
+  },
+  {
+    "revision": "b3627084d31b825426ca23a2fba09431",
+    "url": "/react-to-do-list/index.html"
+  }
+];
